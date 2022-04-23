5e44c4338076d2af824e133f96fc82690d2564a9
CeloRocha <marcelo.ds.rocha77@gmail.com>
2022-04-23 07:03:05 -0300

    test: 💍 More tests for Task component
    Adding tests for all buttons on Task component, mark complete, edit and
    remove.

--- src/components/Task/Task.test.jsx
+++ src/components/Task/Task.test.jsx
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import Task from '.';
 
 const mock = {
@@ -19,5 +19,40 @@ describe('<Task />', () => {
         expect(
             screen.getByText(/a short description about my task/i),
         ).toBeInTheDocument();
+    });
+    it('should call a function, when remove button is clicked', () => {
+        const fn = jest.fn();
+        const { container } = render(<Task {...mock} remove={fn} />);
+        const removeButton = screen.getByRole('button', { name: /remover/i });
+
+        expect.assertions(3);
+        expect(removeButton).toBeInTheDocument();
+        expect(fn).toBeCalledTimes(0);
+        fireEvent.click(removeButton);
+        expect(fn).toBeCalledTimes(1);
+    });
+    it('should call a function, when mark complete button is clicked', () => {
+        const fn = jest.fn();
+        const { container } = render(<Task {...mock} markComplete={fn} />);
+        const completeButton = screen.getByRole('button', {
+            name: /completar/i,
+        });
+
+        expect.assertions(3);
+        expect(completeButton).toBeInTheDocument();
+        expect(fn).toBeCalledTimes(0);
+        fireEvent.click(completeButton);
+        expect(fn).toBeCalledTimes(1);
+    });
+    it('should call a function, when edit button is clicked', () => {
+        const fn = jest.fn();
+        const { container } = render(<Task {...mock} edit={fn} />);
+        const editButton = screen.getByRole('button', { name: /editar/i });
+
+        expect.assertions(3);
+        expect(editButton).toBeInTheDocument();
+        expect(fn).toBeCalledTimes(0);
+        fireEvent.click(editButton);
+        expect(fn).toBeCalledTimes(1);
     });
 });
