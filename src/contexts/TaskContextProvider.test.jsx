b32bb5dd1ded6edc05454403e5823c73729b1082
CeloRocha <marcelo.ds.rocha77@gmail.com>
2022-04-23 18:52:15 -0300

    test: 💍 Add test edit task function

--- src/contexts/TaskContextProvider.test.jsx
+++ src/contexts/TaskContextProvider.test.jsx
@@ -29,13 +29,31 @@ const CustomRender = () => {
             <button onClick={() => taskFunctions.addTask(mockTask)}>
                 Add Task
             </button>
+            <button
+                onClick={() =>
+                    taskFunctions.editTask(
+                        0,
+                        'New Task Title',
+                        'New Task Description',
+                        false,
+                    )
+                }
+            >
+                Edit Task
+            </button>
+            <button onClick={() => taskFunctions.completeTask(0)}>
+                Complete Task
+            </button>
+            <button onClick={() => taskFunctions.removeTask(0)}>
+                Remove Task
+            </button>
         </>
     );
 };
 
 describe('Context: Task Provider', () => {
     it('should add a task to tasks state.', () => {
-        const { debug, container } = ServeContext();
+        ServeContext();
 
         const addButton = screen.getByRole('button', { name: /add task/i });
         expect.assertions(4);
@@ -51,5 +69,29 @@ describe('Context: Task Provider', () => {
         expect(
             screen.queryAllByRole('heading', { name: /task title/i }),
         ).toHaveLength(2);
+    });
+    it('should edit a task, given the correct Id, title and description', () => {
+        const { debug } = ServeContext();
+
+        const addButton = screen.getByRole('button', { name: /add task/i });
+        const editButton = screen.getByRole('button', { name: /edit task/i });
+        expect.assertions(6);
+        expect(editButton).toBeInTheDocument();
+        fireEvent.click(addButton);
+        expect(
+            screen.getByRole('heading', { name: /task title/i }),
+        ).toBeInTheDocument();
+        expect(
+            screen.queryAllByRole('heading', { name: /task title/i }),
+        ).toHaveLength(1);
+        fireEvent.click(editButton);
+        expect(
+            screen.queryAllByRole('heading', { name: /task title/i }),
+        ).toHaveLength(1);
+        expect(
+            screen.getByRole('heading', { name: /new task title/i }),
+        ).toBeInTheDocument();
+
+        expect(screen.getByText(/new task description/i)).toBeInTheDocument();
     });
 });
