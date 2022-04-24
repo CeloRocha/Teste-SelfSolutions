0ce53f0f72e293701d88d6db1f610b329d4dcbc8
CeloRocha <marcelo.ds.rocha77@gmail.com>
2022-04-24 16:55:38 -0300

    feat: 🎸 <Input /> component complete
    Input component already made with style.

--- src/components/Input/index.jsx
+++ src/components/Input/index.jsx
@@ -1,7 +1,26 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import './styles.scss';
 
-const Input = () => {
-    return <div>Input</div>;
+const Input = (props) => {
+    return (
+        <div className={`input ${props.value != '' ? 'focus' : ''}`}>
+            <input
+                id={props.label}
+                type="text"
+                placeholder=""
+                value={props.value}
+                onChange={props.onChange}
+            />
+            <label htmlFor={props.label}>{props.label}</label>
+        </div>
+    );
+};
+
+Input.propTypes = {
+    label: PropTypes.string,
+    value: PropTypes.string,
+    onChange: PropTypes.func,
 };
 
 export default Input;
