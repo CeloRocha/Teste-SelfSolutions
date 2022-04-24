e7afd509f17ea2114ad7c0f0fa744581b6c29b54
CeloRocha <marcelo.ds.rocha77@gmail.com>
2022-04-24 05:00:12 -0300

    feat: 🎸 <Modal /> component complete, withou styles
    Modal is structured to be used in other components, receiving childrens
    in it. Making it a good use in many situations.

--- src/components/Modal/index.jsx
+++ src/components/Modal/index.jsx
@@ -1,7 +1,32 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import './styles.scss';
 
-const Modal = () => {
-    return <div>Modal</div>;
+const Modal = (props) => {
+    return (
+        <>
+            {props.show && (
+                <div className="modal">
+                    <div
+                        className="modal-overlay"
+                        onClick={props.closeModal}
+                    ></div>
+                    <div className="modal-content">
+                        <button onClick={props.closeModal}>Close</button>
+                        <h2 className="modal-title">{props.title}</h2>
+                        {props.children}
+                    </div>
+                </div>
+            )}
+        </>
+    );
+};
+
+Modal.propTypes = {
+    show: PropTypes.bool,
+    title: PropTypes.string,
+    closeModal: PropTypes.func,
+    children: PropTypes.node,
 };
 
 export default Modal;
