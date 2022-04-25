62094871af1124c66c109d7fe380bc7e64d30e44
CeloRocha <marcelo.ds.rocha77@gmail.com>
2022-04-25 09:15:12 -0300

    feat: 🎸 <Modal /> now is animated

--- src/components/Modal/index.jsx
+++ src/components/Modal/index.jsx
@@ -2,24 +2,61 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './styles.scss';
 import Button from '../Button';
+import { motion, AnimatePresence } from 'framer-motion';
+
+const dropIn = {
+    hidden: {
+        y: '-70vh',
+        opacity: 0,
+    },
+    visible: {
+        y: '0',
+        opacity: 1,
+        transition: {
+            duration: 0.3,
+            type: 'spring',
+            damping: 25,
+            stiffness: 500,
+        },
+    },
+    exit: {
+        y: '70vh',
+        opacity: 0,
+    },
+};
 
 const Modal = (props) => {
     return (
-        <>
+        <AnimatePresence
+            initial={true}
+            exitBeforeEnter={true}
+            onExitComplete={() => null}
+        >
             {props.show && (
-                <div className="modal">
+                <motion.div
+                    className="modal"
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                    exit={{ opacity: 0 }}
+                >
                     <div
                         className="modal-overlay"
                         onClick={props.closeModal}
                     ></div>
-                    <div className="modal-content">
+                    <motion.div
+                        className="modal-content"
+                        variants={dropIn}
+                        initial="hidden"
+                        animate="visible"
+                        exit="exit"
+                    >
                         <Button type="close" onClick={props.closeModal} />
                         <h2 className="modal-title">{props.title}</h2>
                         {props.children}
-                    </div>
-                </div>
+                    </motion.div>
+                </motion.div>
             )}
-        </>
+        </AnimatePresence>
     );
 };
 
