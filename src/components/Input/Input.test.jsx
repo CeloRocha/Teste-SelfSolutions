c8bd477b709f8a02ba67d08d3513d5c1b670965c
CeloRocha <marcelo.ds.rocha77@gmail.com>
2022-04-27 00:59:48 -0300

    test: 💍 <Input /> onChange call function

--- src/components/Input/Input.test.jsx
+++ src/components/Input/Input.test.jsx
@@ -1,11 +1,18 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import Input from '.';
-import userEvent from '@testing-library/user-event';
 
 describe('<Input /> component', () => {
     it('should render the input component', () => {
         render(<Input label="Input" />);
 
         expect(screen.getByLabelText(/input/i)).toBeInTheDocument();
+    });
+    it('should call a function when the input change', () => {
+        const fn = jest.fn();
+        render(<Input label="Input" onChange={fn} />);
+        const titleInput = screen.getByLabelText(/input/i);
+        expect(fn).not.toHaveBeenCalled();
+        fireEvent.change(titleInput, { target: { value: 'Edited title' } });
+        expect(fn).toHaveBeenCalled();
     });
 });
